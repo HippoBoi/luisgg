53f6baf77e60423966bad0065227903fdb79de9d
HippoBoi <tagrifavc@gmail.com>
2024-05-02 16:16:17 -0400

    i hate this

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,7 @@
-export const { VITE_API_HOST: API_HOST } = import.meta.env
+declare global {
+    interface ImportMetaEnv {
+        VITE_API_HOST: string;
+    }
+}
+
+export const { VITE_API_HOST: API_HOST } = import.meta.env;
